3046be3e02e36935af6871a5c8cf3e01c77273a4
Giuliano Decesares <giulianodecesares@gmail.com>
2020-04-14 23:43:47 -0300

    Initial commit with setup and a couple of scripts

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private uint memoryConsumptionInBytes = 10;
+    private int[] memoryConsumption;
+
+    private void OnEnable()
+    {
+        this.EatRam(this.memoryConsumptionInBytes);
+    }
+
+    private void EatRam(uint bytes)
+    {
+        this.memoryConsumption = new int[bytes];
+
+        for (int index = 0; index < this.memoryConsumption.Length; index++)
+            this.memoryConsumption[index] = index;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
